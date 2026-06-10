94a3bf83c9cb9a902a83ab789c364a54b1a879bf
jianghh <2503338074@qq.com>
2026-06-10 11:30:17 +0800

    utf-8支持

---
+++ plugins/lua/网络监测.lua
@@ -0,0 +1,28 @@
+name="网络监测"
+sample="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+interval=3
+
+function onUpdate()
+    -- 使用插件提供的 tf.runCmdLine() 函数
+    -- 不会弹出终端窗口，同步执行，等待命令完成后返回
+    local cmd = "set PYTHONIOENCODING=utf-8 && python test_wall.py"
+    
+    local out = tf.runCmdLine(cmd)
+    
+    out = out:gsub("[\r\n]", "")
+    
+    local logFile = io.open("traffic_monitor.log", "a")
+    if logFile then
+        logFile:write(os.date("%H:%M:%S") .. " - 输出: [" .. out .. "]\n")
+        logFile:close()
+    end
+    
+    if out == "" or out == nil then
+        return "网络: 无响应"
+    end
+    
+    return "网络: " .. out
+end
+
+function onClick()
+end
